4dcd2651e23e7b7be0ffa387fa684f8a1b5a3495
taeng0204 <taeng02@icloud.com>
2023-04-06 16:22:07 +0900

    [Refact] Refact DB communication

--- app/main.py
+++ app/main.py
@@ -5,9 +5,23 @@ from datetime import datetime
 app = FastAPI()
 
 @app.get("/")
-def test():
+async def test():
   client = MongoClient("172.19.0.3", 27017)
-  db = client.dbsparta
+  db = client.crawling
   now = datetime.now()
-  data = list(db.users.find({}))
-  return {"time": now, "db": data}
+  return {"time": now, "collections": db.list_collection_names()}
+
+
+@app.get("/collection/{collection_name}")
+async def test(collection_name):
+  client = MongoClient("localhost", 27017)
+  db = client.crawling
+  now = datetime.now()
+  collection = db[collection_name]
+
+  docs = list()
+  for doc in collection.find():
+    doc["_id"] = str(doc["_id"])
+    docs.append(doc)
+
+  return {"time": now, "data": docs}
